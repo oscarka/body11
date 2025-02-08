09eb6d7631b12bab3408ec418b768e6c6288cc03
oscarka <ka-0724@hotmail.com>
2025-02-08 09:39:58 +0800

    Delete testsql directory

--- testsql/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("RAILWAY_SECRET_KEY", "dev")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("RAILWAY_DATABASE_URL", "sqlite:///app.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
